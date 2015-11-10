9671b2ce4d6e08cd629df6fc6c717a9ada5fc4d7
Constantin Shalnev <c.shalnev@corp.mail.ru>
2015-11-10 17:22:21 +0300

    Added key to enable render debug

--- iphone/Maps/Classes/CustomViews/MapViewControls/Search/Console/MWMConsole.mm
+++ iphone/Maps/Classes/CustomViews/MapViewControls/Search/Console/MWMConsole.mm
@@ -3,6 +3,10 @@
 
 #include "Framework.h"
 
+#ifdef BUILD_DESIGNER
+#import "drape/debug_rect_renderer.hpp"
+#endif // BUILD_DESIGNER
+
 extern NSString * const kMwmTextToSpeechEnable;
 extern NSString * const kMwmTextToSpeechDisable;
 
@@ -12,12 +16,30 @@ extern NSString * const kMwmTextToSpeechDisable;
 {
   if ([self performMapStyle:cmd])
     return YES;
-
+  
   if ([self performSound:cmd])
     return YES;
-
+  
+#ifdef BUILD_DESIGNER
+  if ([self performRenderDebug:cmd])
+    return YES;
+#endif
+  
   return NO;
 }
+
+#ifdef BUILD_DESIGNER
++ (BOOL)performRenderDebug:(NSString *)cmd
+{
+  BOOL const isDebug = [cmd isEqualToString:@"?debug"];
+  
+  if (!isDebug)
+    return NO;
+  
+  dp::DebugRectRenderer::Instance().SetEnabled(!dp::DebugRectRenderer::Instance().IsEnabled());
+  return YES;
+}
+#endif // BUILD_DESIGNER
 
 + (BOOL)performMapStyle:(NSString *)cmd
 {
@@ -25,13 +47,13 @@ extern NSString * const kMwmTextToSpeechDisable;
   BOOL const isDark = [cmd isEqualToString:@"mapstyle:dark"] || [cmd isEqualToString:@"?dark"];
   BOOL const isLight = isDark ? NO : [cmd isEqualToString:@"mapstyle:light"] || [cmd isEqualToString:@"?light"];
   BOOL const isOld = isLight || isDark ? NO : [cmd isEqualToString:@"?oldstyle"];
-
+  
   if (!isDark && !isLight && !isOld)
     return NO;
-
+  
   MapStyle const mapStyle = isDark ? MapStyleDark : (isOld ? MapStyleLight : MapStyleClear);
   [[MapsAppDelegate theApp] setMapStyle: mapStyle];
-
+  
   return YES;
 }
 
@@ -40,15 +62,15 @@ extern NSString * const kMwmTextToSpeechDisable;
   // Hook for shell command on change map style
   BOOL const sound = [cmd isEqualToString:@"?sound"];
   BOOL const nosound = sound ? NO : [cmd isEqualToString:@"?nosound"];
-
+  
   if (!sound && !nosound)
     return NO;
-
+  
   if (sound)
     [[NSNotificationCenter defaultCenter] postNotificationName:kMwmTextToSpeechEnable object:nil];
   if (nosound)
     [[NSNotificationCenter defaultCenter] postNotificationName:kMwmTextToSpeechDisable object:nil];
-
+  
   return YES;
 }
 
